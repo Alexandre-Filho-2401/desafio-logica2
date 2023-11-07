913c9af499dde2f24985e369d702e074f78976d6
Alexandre-Filho-2401 <alexandrefilho2401@gmail.com>
2023-11-06 22:24:39 -0300

    testando hipotese de contagem

--- desafio2.js
+++ desafio2.js
@@ -1,4 +1,17 @@
 //Segundo desafio de Lógica de Programação
 //Devo desenvolver uma calculadora de relação de vitórias com base nas instruções dadas
 
+let wins = 0;
+let rank = 5;
+
+function partida(){
+    let cont=math.floor(math.random()*2);
+    if(cont=1){
+        wins+=1;
+    }
+}
+for(let i=1;i<=100;i++) {
+    partida();
+}
+
 console.log("O Herói tem o saldo "+wins+" de vitórias e está no nível "+rank);

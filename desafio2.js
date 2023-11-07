e7416f029512fbfc34267ded3e6d8107c618415a
Alexandre-Filho-2401 <alexandrefilho2401@gmail.com>
2023-11-07 20:51:21 -0300

    Desafio concluido

--- desafio2.js
+++ desafio2.js
@@ -1,11 +1,36 @@
 //Segundo desafio de Lógica de Programação
 //Devo desenvolver uma calculadora de relação de vitórias com base nas instruções dadas
 
+//A função gera um numero de vitórias qualquer com base em 100 partidas
 function contadorPartidas() {
     let wins = Math.floor(Math.random()*100);
     let partidas = 100;
     return partidas - wins;
 }
-let wins = contadorPartidas();
+let wins=contadorPartidas();
+let rank;
+
+switch(true){
+    case wins<=10:
+        rank="Ferro";
+        break
+    case wins<=20:
+        rank="Bronze";
+        break
+    case wins<=50:
+        rank="Prata";
+        break
+    case wins<=80:
+        rank="Ouro"
+        break
+    case wins<=90:
+        rank="Diamante";
+        break
+    case wins<=100:
+        rank="Lendario";
+        break
+    default:
+        rank="Imortal";
+}
 
 console.log("O Herói tem o saldo "+wins+" de vitórias e está no nível "+rank);

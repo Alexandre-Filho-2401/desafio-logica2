65e772dfcaf35e0056a6ae4a2beb1bbf7218dc35
Alexandre-Filho-2401 <alexandrefilho2401@gmail.com>
2023-11-07 20:41:17 -0300

    testando vitórias aleatórias

--- desafio2.js
+++ desafio2.js
@@ -1,17 +1,11 @@
 //Segundo desafio de Lógica de Programação
 //Devo desenvolver uma calculadora de relação de vitórias com base nas instruções dadas
 
-let wins = 0;
-let rank = 5;
-
-function partida(){
-    let cont=math.floor(math.random()*2);
-    if(cont=1){
-        wins+=1;
-    }
+function contadorPartidas() {
+    let wins = Math.floor(Math.random()*100);
+    let partidas = 100;
+    return partidas - wins;
 }
-for(let i=1;i<=100;i++) {
-    partida();
-}
+let wins = contadorPartidas();
 
 console.log("O Herói tem o saldo "+wins+" de vitórias e está no nível "+rank);

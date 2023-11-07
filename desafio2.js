27df693b8ff3ee5f586c0da762ea3931e03b5b2b
Alexandre-Filho-2401 <alexandrefilho2401@gmail.com>
2023-11-06 22:17:18 -0300

    Abrindo o desafio

--- desafio2.js
+++ desafio2.js
@@ -1,1 +1,4 @@
 //Segundo desafio de Lógica de Programação
+//Devo desenvolver uma calculadora de relação de vitórias com base nas instruções dadas
+
+console.log("O Herói tem o saldo "+wins+" de vitórias e está no nível "+rank);
